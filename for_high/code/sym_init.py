4a868f8fd6e9056be3155fb4e3776ebad3d737fe
ducksfrogs <ma_yamaki@yahoo.com>
2022-09-10 15:59:56 +0900

    dd

--- for_high/code/sym_init.py
+++ for_high/code/sym_init.py
@@ -3,6 +3,15 @@ from sympy import symbols
 
 from sympy import sqrt
 from sympy import exp
+from sympy import cos, sin
+
+from sympy import subs
+
+from sympy import simplify
 
 
 from sympy import Integral
+
+
+init_printing()
+x, y, z = symbols('x y z')
